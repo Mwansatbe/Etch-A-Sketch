54a8573b420859df6c54a9a73c7b31aea13cbd3e
Mwansa Chilekwa <mwansachille@gmail.com>
2022-08-26 01:28:53 +0200

    Add change color event on gridItem when mouseover

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
+// Select target element
 const container = document.querySelector(".grid-container");
 console.log(container);
 
+// Creates the grid
 function makeGrid(rows, columns){
 let num =1;
 for(let i =0; i<(rows * columns); i++){
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-columns', columns);
     const gridItem= document.createElement("div");
     gridItem.classList.add("grid-item");
     gridItem.innerText='';
     console.log(gridItem)
     container.appendChild(gridItem);
+    gridItem.addEventListener("mouseover", ()=>{
+        gridItem.style.backgroundColor="black";
+    })
 
 }
 }
