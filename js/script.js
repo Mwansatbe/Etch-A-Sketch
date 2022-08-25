ab3686b29fdc3f638bbf5da08464a9a0ef49e977
Mwansa Chilekwa <mwansachille@gmail.com>
2022-08-25 23:35:13 +0200

    Create function that formulates grid

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 const container = document.querySelector(".grid-container");
 console.log(container);
+
+function makeGrid(rows, columns){
+let num =1;
+for(let i =0; i<(rows * columns); i++){
+    const gridItem= document.createElement("div");
+    gridItem.classList.add("grid-item");
+    gridItem.innerText=num+i;
+    console.log(gridItem)
+    container.appendChild(gridItem);
+
+}
+}
+makeGrid(16,16)
